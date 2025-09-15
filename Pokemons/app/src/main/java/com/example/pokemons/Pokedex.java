09e90cfb65a1975a6a41cf1cc30cfdb30223035f
MrHerbat <kocirel206@gmail.com>
2025-09-15 14:36:30 +0200

    Started new mobile app - Pokemons
    TODO: add more types, complete Pokedex.java and MainActivity.java

---
+++ Pokemons/app/src/main/java/com/example/pokemons/Pokedex.java
@@ -0,0 +1,16 @@
+package com.example.pokemons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pokedex
+{
+    List<String> getPokemons(String pokemonType){
+        List<String> pokemonList = new ArrayList<String>();
+        switch (pokemonType){
+
+        }
+
+        return pokemonList;
+    }
+}
