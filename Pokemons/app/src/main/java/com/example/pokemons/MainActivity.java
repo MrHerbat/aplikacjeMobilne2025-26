37bed932f46820a390ccdeb56a48d1709f47c5fe
MrHerbat <kocirel206@gmail.com>
2025-09-16 14:55:45 +0200

    Skończony projekt - Pokemons!
    chyba już nie ma nic do dodania tutaj :3

--- Pokemons/app/src/main/java/com/example/pokemons/MainActivity.java
+++ Pokemons/app/src/main/java/com/example/pokemons/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pokemons;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private Pokedex pokedex = new Pokedex();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            TextView pokemons = (TextView) findViewById(R.id.pokemonDisplay);
+            pokemons.setMovementMethod(new ScrollingMovementMethod());
             return insets;
         });
+    }
+    public void onClickFindPokemons(View view){
+        TextView pokemons = (TextView) findViewById(R.id.pokemonDisplay);
+        Spinner baseType = (Spinner) findViewById(R.id.pokemonBaseType);
+        Spinner addType = (Spinner) findViewById(R.id.pokemonAddType);
+        List<String> pokemonList;
+        pokemonList = pokedex.getPokemons(baseType.getSelectedItem().toString(),addType.getSelectedItem().toString());
+
+
+        String pokemonsFormated = "";
+        for (int i = 0; i < pokemonList.size(); i++) {
+            pokemonsFormated+=(pokemonList.get(i)+"\n");
+        }
+        pokemons.setText(pokemonsFormated);
     }
 }
