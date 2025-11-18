f7a4b95a3b9e3ca833f18715ff45d5f73ceab0c4
MrHerbat <kocirel206@gmail.com>
2025-11-18 12:36:41 +0100

    TODO:CRAFTING

---
+++ CraftingGrid/app/src/main/java/com/example/craftinggrid/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.craftinggrid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String pickedItem = " ";
+    private char[][] craftingGrid = new char[3][3];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void setClickedButtonItem(View view) {
+        if(!pickedItem.equals(" ")){
+            Button clickedButton = (Button) findViewById(view.getId());
+            System.out.println(view.getId());
+            if(pickedItem.equals(clickedButton.getText().toString())){
+                clickedButton.setText(" ");
+            }else {
+                clickedButton.setText(pickedItem);
+            }
+        }
+    }
+
+    public void pickItem(View view) {
+        Button clickedButton = (Button) findViewById(view.getId());
+        pickedItem=clickedButton.getText().toString();
+    }
+}
