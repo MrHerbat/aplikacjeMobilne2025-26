ac57a5b26d62882c04364706ce55da13c48abf13
MrHerbat <kocirel206@gmail.com>
2026-03-24 14:05:40 +0100

    listy updated

--- Listy/app/src/main/java/com/example/listy/DrinkActivity.java
+++ Listy/app/src/main/java/com/example/listy/DrinkActivity.java
@@ -1,9 +1,30 @@
 package com.example.listy;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
     public static final String EXTRA_DRINKID = "drinkId";
-    int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
-    Drink drink = Drink.drinks[drinkId];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView name = findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView desc = findViewById(R.id.description);
+        desc.setText(drink.getDesc());
+
+        ImageView photo = findViewById(R.id.photo);
+        photo.setImageResource(drink.getImgResId());
+        photo.setContentDescription(drink.getName());
+    }
 }
